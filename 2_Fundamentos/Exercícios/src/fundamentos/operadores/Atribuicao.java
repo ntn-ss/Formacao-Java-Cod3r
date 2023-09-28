26ebd7463dbdcff47244e97591ab4017f2f1f81b
Nathan <nathandossantosdasilva@gmail.com>
2023-09-28 14:36:36 -0300

    Ajuste Operadores de Atribuição

--- 2_Fundamentos/Exercícios/src/fundamentos/operadores/Atribuicao.java
+++ 2_Fundamentos/Exercícios/src/fundamentos/operadores/Atribuicao.java
@@ -7,12 +7,17 @@ public class Atribuicao {
         int c = a + b;
 
         c += b; //c = c + b
-        c -= a; //c = c - a
-        c *= b; //c = c * b
-        c /= a; //c = c / b
-
         System.out.println(c);
         
+        c -= a; //c = c - a
+        System.out.println(c);
+        
+        c *= b; //c = c * b
+        System.out.println(c);
+        
+        c /= a; //c = c / a
+        System.out.println(c);
+
         // Isto só retornará zero ou um: zero se par, um se ímpar. Saber se é par ou ímpar é o uso mais comum do resto de uma divisão.
         c %= 2; //c = c % 2
         
